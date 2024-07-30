7050d0964c3f7ffd943c70fe4e433df07ee9cb2f
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-07-30 10:51:56 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from routes.home import home_page
+from routes.grades import grades_page
+from routes.courses import courses_page
+from api_routes.grades import grades_api
+from api_routes.courses import courses_api
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+app.register_blueprint(home_page)
+app.register_blueprint(grades_page)
+app.register_blueprint(courses_page)
+app.register_blueprint(grades_api)
+app.register_blueprint(courses_api)
+
+print(app.url_map)
+
+from werkzeug.exceptions import HTTPException
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    if isinstance(e, HTTPException):
+        return e
+
+    else:
+        return render_template("500.html", error_message=e), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
