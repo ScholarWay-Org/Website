ee74eb07100f3bf18577c3f40f1991bd59ba0266
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-07-30 16:42:28 -0400

    Add files via upload

---
+++ routes/grades.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+grades_page = Blueprint('grades_page', __name__, template_folder='templates')
+
+# Created Possible Routes for the Grades Page
+
+@grades_page.route('/grades')
+def grades_view():
+    return render_template('templates_english/grades_home.html')
+
+@grades_page.route('/grades/gpa/calculator')
+def gpa_calculator_view():
+    return render_template('templates_english/grades/gpa.html')
+
+@grades_page.route('/grades/fgc')
+def final_grade_calculator_view():
+    return render_template('templates_english/grades/finalgrade.html')
